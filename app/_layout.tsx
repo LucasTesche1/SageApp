a80e49c15f825dfd53b33e4755788b3cdd83b72f
LucasTesche1 <lucas@yujilive.com.br>
2025-11-04 21:04:24 -0300

    api integration to medicine page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -31,6 +31,7 @@ export default function RootLayout() {
         <Stack.Screen name="cardEdit" options={{ headerShown: false }} />
         <Stack.Screen name="faq" options={{ headerShown: false }} />
         <Stack.Screen name="deliveryTracking" options={{ headerShown: false }} />
+        <Stack.Screen name="remedioPage" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
       <StatusBar style="auto" />
--- app/medicine.tsx
+++ app/medicine.tsx
@@ -1,28 +1,50 @@
-
 import { Ionicons } from "@expo/vector-icons";
 import { router } from "expo-router";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Image, Pressable, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
-
+import { getAllItems, Item } from "../services/Itens"; // importa seu service
 
 export default function HomeScreen() {
+  const [itens, setItens] = useState<Item[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function carregarItens() {
+      try {
+        const data = await getAllItems();
+        setItens(data);
+        console.log(data);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    carregarItens();
+  }, []);
+
+  const imagens = [
+    require("@/assets/images/remedios/1.png"),
+    require("@/assets/images/remedios/2.jpg"),
+    require("@/assets/images/remedios/3.jpg"),
+    require("@/assets/images/remedios/4.jpg"),
+  ];
 
   return (
-
     <ScrollView style={styles.container}>
-
-    <View style={styles.container}>
       {/* HEADER */}
       <View style={styles.header}>
         <Text style={styles.logo}>SAGE</Text>
         <TouchableOpacity
           style={styles.logoImg}
-          onPress={() => router.push('/(tabs)')}
+          onPress={() => router.push("/(tabs)")}
         >
           <Image
-            source={require('@/assets/images/image.png')}
-            style={styles.logoImg}/>
-        </TouchableOpacity>        
+            source={require("@/assets/images/image.png")}
+            style={styles.logoImg}
+          />
+        </TouchableOpacity>
       </View>
 
       {/* BARRA DE PESQUISA */}
@@ -31,186 +53,41 @@ export default function HomeScreen() {
           style={styles.input}
           placeholder="O que está procurando?"
         />
-        <Ionicons name="search" size={20} color="#888" style={styles.searchIcon}/>
+        <Ionicons name="search" size={20} color="#888" style={styles.searchIcon} />
       </View>
 
-      {/* GRUPO DOS REMEDIOS */}
+      {/* LISTA DE CARDS DINÂMICOS */}
       <View style={styles.cardsContainer}>
-        <View style={styles.card}>
-          <Image source={require("@/assets/images/remedios/1.png")} style={styles.logoImgRemedio}/>
-          <Text style={styles.cardDescricao}>Lorem Ipson Lorem</Text>
-          <Text style={styles.cardMg}>150 mg</Text>
-          <Pressable
-            style={({ pressed }) => [
-              styles.buttonSee,
-              pressed && { opacity: 0.6 }
-            ]}
-            
-          >
-          <Text style={{ color:'#fff', fontFamily: 'Tahoma', fontWeight:'thin' }}>Ver</Text>
-          </Pressable>
-          <View style={styles.cardBtns}>
-
-          </View>
-
-        </View>
-        <View style={styles.card}>
-          <Image source={require("@/assets/images/remedios/2.jpg")} style={styles.logoImgRemedio}/>
-          <Text style={styles.cardDescricao}>Lorem Ipson Lorem </Text>
-          <Text style={styles.cardMg}>150 mg</Text>
-          <Pressable
-            style={({ pressed }) => [
-              styles.buttonSee,
-              pressed && { opacity: 0.6 }
-            ]}
-            
-          >
-          <Text style={{ color:'#fff', fontFamily: 'Tahoma', fontWeight:'thin' }}>Ver</Text>
-          </Pressable>          
-          <View style={styles.cardBtns}>
-
-          </View>
-
-        </View>
-        <View style={styles.card}>
-          <Image source={require("@/assets/images/remedios/3.jpg")} style={styles.logoImgRemedio}/>
-          <Text style={styles.cardDescricao}>Lorem Ipson Lorem </Text>
-          <Text style={styles.cardMg}>150 mg</Text>
-          <Pressable
-            style={({ pressed }) => [
-              styles.buttonSee,
-              pressed && { opacity: 0.6 }
-            ]}
-            
-          >
-          <Text style={{ color:'#fff', fontFamily: 'Tahoma', fontWeight:'thin' }}>Ver</Text>
-          </Pressable>
-          <View style={styles.cardBtns}>
-
-          </View>
-
-        </View>
-        <View style={styles.card}>
-          <Image source={require("@/assets/images/remedios/4.jpg")} style={styles.logoImgRemedio}/>
-          <Text style={styles.cardDescricao}>Lorem Ipson Lorem </Text>
-          <Text style={styles.cardMg}>150 mg</Text>
-          <Pressable
-            style={({ pressed }) => [
-              styles.buttonSee,
-              pressed && { opacity: 0.6 }
-            ]}
-            
-          >
-          <Text style={{ color:'#fff', fontFamily: 'Tahoma', fontWeight:'thin' }}>Ver</Text>
-          </Pressable>
-          <View style={styles.cardBtns}>
-
-          </View>
-
-        </View>
-                <View style={styles.card}>
-          <Image source={require("@/assets/images/remedios/1.png")} style={styles.logoImgRemedio}/>
-          <Text style={styles.cardDescricao}>Lorem Ipson Lorem </Text>
-          <Text style={styles.cardMg}>150 mg</Text>
-          <Pressable
-            style={({ pressed }) => [
-              styles.buttonSee,
-              pressed && { opacity: 0.6 }
-            ]}
-            
-          >
-          <Text style={{ color:'#fff', fontFamily: 'Tahoma', fontWeight:'thin' }}>Ver</Text>
-          </Pressable>
-          <View style={styles.cardBtns}>
-
-          </View>
-
-        </View>
-                <View style={styles.card}>
-          <Image source={require("@/assets/images/remedios/2.jpg")} style={styles.logoImgRemedio}/>
-          <Text style={styles.cardDescricao}>Lorem Ipson Lorem </Text>
-          <Text style={styles.cardMg}>150 mg</Text>
-          <Pressable
-            style={({ pressed }) => [
-              styles.buttonSee,
-              pressed && { opacity: 0.6 }
-            ]}
-            
-          >
-          <Text style={{ color:'#fff', fontFamily: 'Tahoma', fontWeight:'thin' }}>Ver</Text>
-          </Pressable>
-          <View style={styles.cardBtns}>
-
-          </View>
-
-        </View>
-                <View style={styles.card}>
-          <Image source={require("@/assets/images/remedios/3.jpg")} style={styles.logoImgRemedio}/>
-          <Text style={styles.cardDescricao}>Lorem Ipson Lorem </Text>
-          <Text style={styles.cardMg}>150 mg</Text>
-          <Pressable
-            style={({ pressed }) => [
-              styles.buttonSee,
-              pressed && { opacity: 0.6 }
-            ]}
-            
-          >
-          <Text style={{ color:'#fff', fontFamily: 'Tahoma', fontWeight:'thin' }}>Ver</Text>
-          </Pressable>
-          <View style={styles.cardBtns}>
-
-          </View>
-
-        </View>
-                <View style={styles.card}>
-          <Image source={require("@/assets/images/remedios/4.jpg")} style={styles.logoImgRemedio}/>
-          <Text style={styles.cardDescricao}>Lorem Ipson Lorem </Text>
-          <Text style={styles.cardMg}>150 mg</Text>
-          <Pressable
-            style={({ pressed }) => [
-              styles.buttonSee,
-              pressed && { opacity: 0.6 }
-            ]}
-            
-          >
-          <Text style={{ color:'#fff', fontFamily: 'Tahoma', fontWeight:'thin' }}>Ver</Text>
-          </Pressable>
-          <View style={styles.cardBtns}>
-
-          </View>
-
-        </View>
-                <View style={styles.card}>
-          <Image source={require("@/assets/images/remedios/1.png")} style={styles.logoImgRemedio}/>
-          <Text style={styles.cardDescricao}>Lorem Ipson Lorem </Text>
-          <Text style={styles.cardMg}>150 mg</Text>
-          <Pressable
-            style={({ pressed }) => [
-              styles.buttonSee,
-              pressed && { opacity: 0.6 }
-            ]}
-            
-          >
-          <Text style={{ color:'#fff', fontFamily: 'Tahoma', fontWeight:'thin' }}>Ver</Text>
-          </Pressable>
-          <View style={styles.cardBtns}>
-
-          </View>
-
-        </View>
+        {loading ? (
+          <Text style={{ textAlign: "center", marginTop: 20 }}>Carregando...</Text>
+        ) : (
+          itens.map((item, index) => (
+            <View key={item.id} style={styles.card}>
+              <Image
+                source={imagens[index % imagens.length]} // alterna as imagens
+                style={styles.logoImgRemedio}
+              />
+              <Text style={styles.cardDescricao}>{item.name}</Text>
+              <Text style={styles.cardMg}>{item.dosage}</Text>
+              <Pressable
+                style={({ pressed }) => [
+                  styles.buttonSee,
+                  pressed && { opacity: 0.6 },
+                ]}
+                onPress={() => router.push("/remedioPage")}
+              >
+                <Text style={{ color: "#fff", fontFamily: "Tahoma" }}>Ver</Text>
+              </Pressable>
+            </View>
+          ))
+        )}
       </View>
-    </View>
-
     </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: { 
-    flex: 1,
-    backgroundColor: "#fff",
-
-  },
+  container: { flex: 1, backgroundColor: "#fff" },
 
   header: {
     flexDirection: "row",
@@ -218,23 +95,22 @@ const styles = StyleSheet.create({
     alignItems: "center",
     paddingHorizontal: 5,
     paddingVertical: 5,
-    marginTop: 15
+    marginTop: 15,
   },
-  
-  logo: { 
+
+  logo: {
     fontSize: 40,
     color: "#00CED1",
     fontFamily: "Michroma",
-    paddingLeft : 20
+    paddingLeft: 20,
   },
 
   logoImg: {
     width: 70,
     height: 70,
     resizeMode: "contain",
-    paddingRight : 60,
-    top:1
   },
+
   searchBox: {
     flexDirection: "row",
     alignItems: "center",
@@ -246,15 +122,8 @@ const styles = StyleSheet.create({
     paddingHorizontal: 10,
   },
 
-  input: { 
-    flex: 1,
-    padding: 8,
-    color: '#838383ff'
-  },
-
-  searchIcon: { 
-    marginLeft: 5 
-  },
+  input: { flex: 1, padding: 8, color: "#838383ff" },
+  searchIcon: { marginLeft: 5 },
 
   cardsContainer: {
     flexDirection: "row",
@@ -267,24 +136,22 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: "#d2d2d2",
     width: "48%",
-    height: "39%",
     marginBottom: 15,
     borderRadius: 10,
-    gap:20,
     padding: 20,
     alignItems: "center",
   },
 
-    logoImgRemedio: {
+  logoImgRemedio: {
     width: "70%",
-    height: "50%",
+    height: 100,
     resizeMode: "contain",
   },
 
   cardDescricao: {
     fontSize: 18,
     fontWeight: "400",
-    textAlign: "center", 
+    textAlign: "center",
     color: "rgba(0, 0, 0, 1)",
   },
 
@@ -292,28 +159,13 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontWeight: "400",
     color: "#847E7E",
-    fontStyle: "italic"
+    fontStyle: "italic",
   },
 
-  cardBtns:{
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-  },
-
-  button: {
-    paddingHorizontal: 5,
-  },
-  value: {
-    fontSize: 18,
-    fontWeight: "500",
-    color: "#000",
-  },
-
-  buttonSee:{
-    backgroundColor: '#C80000',
+  buttonSee: {
+    backgroundColor: "#C80000",
     padding: 10,
-    paddingHorizontal:20,
-    borderRadius: 30
-  }
+    paddingHorizontal: 20,
+    borderRadius: 30,
+  },
 });
